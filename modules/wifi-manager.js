23d091605b54691dd36170dad7b31fe886ad8b0e
Louis Orleans <louis@orleans.io>
2016-04-28 17:16:18 -0700

    Testing to see if AP started

--- modules/wifi-manager.js
+++ modules/wifi-manager.js
@@ -19,7 +19,7 @@ module.exports = function () {
         accessPoint: {
             status: 'down',
             up: function (SSID, password) {
-                return new Promise(function (resolve) {
+                return new Promise(function (resolve, reject) {
                     console.log('Turning AP on');
                     console.log('SSID: ' + SSID);
                     console.log('password: ' + password);
@@ -54,31 +54,18 @@ module.exports = function () {
                         interfaces = interfaces.replaceAll('{{hostapd}}', hostapdPath);
                         fs.writeFileSync(interfacesPath, interfaces);
 
-                        // exec('ifconfig wlan0 up', function (error, stdout, stderr) {
-                        //     exec('iwconfig wlan0 mode ad-hoc', function (error, stdout, stderr) {
-                        //         exec('iwconfig wlan0 essid ' + SSID, function (error, stdout, stderr) {
-                        //             exec('ifconfig wlan0 192.168.1.1 netmask 255.255.255.0', function (error, stdout, stderr) {
-                        //                 console.log(error + '\n' + stdout + '\n' + stderr);
-                        //                 // fs.writeFileSync('./start_ap', 'ifconfig wlan0 up 192.168.5.1 netmask 255.255.255.0\nsleep 5\nif [ \'$(ps | grep udhcpd)\' == \'\' ]; then\nudhcpd wlan0 &\nfi\nsleep 2\nhostapd /etc/hostapd/hostapd.conf 1>/dev/null\nkillall udhcpd');
-                        //                 // fs.chmodSync('./start_ap', 0777);
-                        //                 // exec('./start_ap', function (error, stdout, stderr) {
-                        //                 //   console.log(error + '\n' + stdout + '\n' + stderr);
-                        //                     if (callbacks.onAPstart) {
-                        //                         callbacks.onAPstart(SSID, password);
-                        //                     }
-                        //                 // });
-                        //             }
-                        //         }
-                        //     }
-                        // }
-
                         exec('/etc/init.d/hostapd restart', function (err, stdout) {
                             if (err) {
                                 console.log(err);
                             } else {
                                 console.log(stdout);
-                                wifi.accessPoint.status = 'up';
-                                resolve({SSID: SSID, password: password});
+                                if (stdout.indexOf('Starting advanced IEEE 802.11 management: hostapd.') > -1) {
+                                    wifi.accessPoint.status = 'up';
+                                    resolve({SSID: SSID, password: password});
+                                } else {
+                                    wifi.accessPoint.status = 'down';
+                                    reject();
+                                }
                             }
                         });
                     }

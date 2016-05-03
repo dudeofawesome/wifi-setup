5806cb3ea3f8a35550062644b5f93ac779f75a02
Louis Orleans <louis@orleans.io>
2016-05-03 13:01:23 -0700

    Promisifying
    Really, I promise!

--- modules/wifi-manager.js
+++ modules/wifi-manager.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var utils = require('./utils');
 
 var noSudoMessage = 'The wifi-setup module requires root permissions in order to modify system config files.\n' +
-    '  Please try running node as root';
+    '    Please try running node as root';
 
 module.exports = function () {
     var wifi = {
@@ -29,26 +29,26 @@ module.exports = function () {
                     if (wifi.client.status !== 'down') {
                         resolve();
                     } else {
-                        wifi.configFiles.all.setAP(SSID, password);
-
-                        exec('/etc/init.d/hostapd restart', function (err, stdout) {
-                            if (err) {
-                                if (err.message.indexOf('Access denied') > -1) {
-                                    console.log(noSudoMessage);
+                        wifi.configFiles.all.setAP(SSID, password).then(function () {
+                            exec('/etc/init.d/hostapd restart', function (err, stdout) {
+                                if (err) {
+                                    if (err.message.indexOf('Access denied') > -1) {
+                                        console.log(noSudoMessage);
+                                    } else {
+                                        console.log(err);
+                                    }
+                                    reject(err);
                                 } else {
-                                    console.log(err);
+                                    console.log(stdout);
+                                    if (stdout.indexOf('Restarting hostapd') > -1) {
+                                        wifi.accessPoint.status = 'up';
+                                        resolve({SSID: SSID, password: password});
+                                    } else {
+                                        wifi.accessPoint.status = 'down';
+                                        reject();
+                                    }
                                 }
-                                reject(err);
-                            } else {
-                                console.log(stdout);
-                                if (stdout.indexOf('Restarting hostapd') > -1) {
-                                    wifi.accessPoint.status = 'up';
-                                    resolve({SSID: SSID, password: password});
-                                } else {
-                                    wifi.accessPoint.status = 'down';
-                                    reject();
-                                }
-                            }
+                            });
                         });
                     }
                 });
@@ -67,7 +67,7 @@ module.exports = function () {
                         } else {
                             console.log('WiFi AP stopped');
                             wifi.accessPoint.status = 'down';
-                            resolve(err);
+                            resolve();
                         }
                     });
                 });
@@ -81,17 +81,17 @@ module.exports = function () {
                     if (wifi.accessPoint.status !== 'down') {
                         reject();
                     } else {
-                        wifi.configFiles.all.setClient(SSID, password);
-
-                        exec('ifup wlan0', function (err, stdout) {
-                            if (err) {
-                                console.log(err);
-                                reject(err);
-                            } else {
-                                console.log(stdout);
-                                wifi.client.status = 'connected';
-                                resolve(stdout);
-                            }
+                        wifi.configFiles.all.setClient(SSID, password).then(function () {
+                            exec('ifup wlan0', function (err, stdout) {
+                                if (err) {
+                                    console.log(err);
+                                    reject(err);
+                                } else {
+                                    console.log(stdout);
+                                    wifi.client.status = 'connected';
+                                    resolve(stdout);
+                                }
+                            });
                         });
                     }
                 });
@@ -120,75 +120,107 @@ module.exports = function () {
                             }
                         } catch (e) {}
 
-                        var defaultHostapdConf = fs.readFileSync('./modules/hostapd.fill').toString();
-                        defaultHostapdConf = defaultHostapdConf.replaceAll('{{path}}', wifi.configFiles.hostapdConf.path);
-                        fs.writeFile(wifi.configFiles.defaultHostapd.path, defaultHostapdConf, function (err) {
-                            if (err) {
-                                reject(err);
-                            } else {
-                                resolve();
-                            }
+                        fs.readFile('./modules/hostapd.fill', function (err, file) {
+                            var defaultHostapdConf = file.toString();
+                            defaultHostapdConf = defaultHostapdConf.replaceAll('{{path}}', wifi.configFiles.hostapdConf.path);
+                            fs.writeFile(wifi.configFiles.defaultHostapd.path, defaultHostapdConf, function (err) {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve();
+                                }
+                            });
                         });
                     });
                 },
                 setClient: function () {
-                    try {
+                    return new Promise(function (resolve) {
                         utils.backupFile(wifi.configFiles.defaultHostapd.path + '.back', function (path) {
                             return path.split('.back')[0];
                         });
-                    } catch (e) {}
+                        resolve();
+                    });
                 }
             },
             hostapdConf: {
                 path: '/etc/hostapd/hostapd.conf',
                 setAP: function (SSID, password) {
-                    try {
-                        if (fs.readFileSync(wifi.configFiles.hostapdConf.path).toString().indexOf('# wifi-setup config') === -1) {
-                            utils.backupFile(wifi.configFiles.hostapdConf.path);
-                        }
-                    } catch (e) {}
+                    return new Promise(function (resolve, reject) {
+                        try {
+                            if (fs.readFileSync(wifi.configFiles.hostapdConf.path).toString().indexOf('# wifi-setup config') === -1) {
+                                utils.backupFile(wifi.configFiles.hostapdConf.path);
+                            }
+                        } catch (e) {}
 
-                    var hostapdConf = fs.readFileSync('./modules/hostapd.conf.fill').toString();
-                    hostapdConf = hostapdConf.replaceAll('{{SSID}}', SSID);
-                    hostapdConf = hostapdConf.replaceAll('{{password}}', password);
-                    hostapdConf = hostapdConf.replaceAll('{{driver}}', 'rtl871xdrv');
-                    fs.writeFileSync(wifi.configFiles.hostapdConf.path, hostapdConf);
+                        fs.readFile('./modules/hostapd.conf.fill', function (err, file) {
+                            var hostapdConf = file.toString();
+                            hostapdConf = hostapdConf.replaceAll('{{SSID}}', SSID);
+                            hostapdConf = hostapdConf.replaceAll('{{password}}', password);
+                            hostapdConf = hostapdConf.replaceAll('{{driver}}', 'rtl871xdrv');
+                            fs.writeFile(wifi.configFiles.hostapdConf.path, hostapdConf, function (err) {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve();
+                                }
+                            });
+                        });
+                    });
                 },
                 setClient: function () {
-                    try {
+                    return new Promise(function (resolve) {
                         utils.backupFile(wifi.configFiles.hostapdConf.path + '.back', function (path) {
                             return path.split('.back')[0];
                         });
-                    } catch (e) {}
+                        resolve();
+                    });
                 }
             },
             interfaces: {
                 path: '/etc/network/interfaces',
                 setAP: function () {
-                    try {
-                        if (fs.readFileSync(wifi.configFiles.interfaces.path).toString().indexOf('# wifi-setup config') === -1) {
-                            utils.backupFile(wifi.configFiles.interfaces.path);
-                        }
-                    } catch (e) {}
-
-                    var interfaces = fs.readFileSync('./modules/interfaces.ap.fill').toString();
-                    interfaces = interfaces.replaceAll('{{IP}}', '192.168.42.1');
-                    interfaces = interfaces.replaceAll('{{hostapd}}', wifi.configFiles.hostapdConf.path);
-                    fs.writeFileSync(wifi.configFiles.interfaces.path, interfaces);
-                },
-                setClient: function (SSID, password) {
-                    try {
+                    return new Promise(function (resolve, reject) {
                         try {
                             if (fs.readFileSync(wifi.configFiles.interfaces.path).toString().indexOf('# wifi-setup config') === -1) {
                                 utils.backupFile(wifi.configFiles.interfaces.path);
                             }
                         } catch (e) {}
 
-                        var interfaces = fs.readFileSync('./modules/interfaces.client.fill').toString();
-                        interfaces = interfaces.replaceAll('{{SSID}}', SSID);
-                        interfaces = interfaces.replaceAll('{{password}}', password);
-                        fs.writeFileSync(wifi.configFiles.interfaces.path, interfaces);
-                    } catch (e) {}
+                        fs.readFile('./modules/interfaces.ap.fill', function (err, file) {
+                            var interfaces = file.toString();
+                            interfaces = interfaces.replaceAll('{{IP}}', '192.168.42.1');
+                            interfaces = interfaces.replaceAll('{{hostapd}}', wifi.configFiles.hostapdConf.path);
+                            fs.writeFile(wifi.configFiles.interfaces.path, interfaces, function (err) {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve();
+                                }
+                            });
+                        });
+                    });
+                },
+                setClient: function (SSID, password) {
+                    return new Promise(function (resolve, reject) {
+                        try {
+                            if (fs.readFileSync(wifi.configFiles.interfaces.path).toString().indexOf('# wifi-setup config') === -1) {
+                                utils.backupFile(wifi.configFiles.interfaces.path);
+                            }
+                        } catch (e) {}
+
+                        fs.readFile('./modules/interfaces.client.fill', function (err, file) {
+                            var interfaces = file.toString();
+                            interfaces = interfaces.replaceAll('{{SSID}}', SSID);
+                            interfaces = interfaces.replaceAll('{{password}}', password);
+                            fs.writeFile(wifi.configFiles.interfaces.path, interfaces, function (err) {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve();
+                                }
+                            });
+                        });
+                    });
                 }
             },
             all: {
@@ -209,9 +241,20 @@ module.exports = function () {
                     });
                 },
                 setClient: function (SSID, password) {
-                    wifi.configFiles.defaultHostapd.setClient();
-                    wifi.configFiles.hostapdConf.setClient();
-                    wifi.configFiles.interfaces.setClient(SSID, password);
+                    return new Promise(function (resolve, reject) {
+                        Promise.all([
+                            wifi.configFiles.defaultHostapd.setClient(),
+                            wifi.configFiles.hostapdConf.setClient(),
+                            wifi.configFiles.interfaces.setClient(SSID, password)
+                        ]).then(function (results) {
+                            resolve(results);
+                        }).catch(function (errs) {
+                            if (errs[0].code === 'EACCES') {
+                                console.log(noSudoMessage);
+                            }
+                            reject(errs);
+                        });
+                    });
                 }
             }
         }

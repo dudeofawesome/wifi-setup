6107f3591ff8e55967222efbdda5d3d8045bb1f1
Louis Orleans <louis@orleans.io>
2016-05-12 18:37:01 -0700

    Removing Should

--- src/modules/tests/database.test.ts
+++ src/modules/tests/database.test.ts
@@ -1,1 +1,33 @@
-import * as database from '../database';
+import * as chai from 'chai';
+import * as fs from 'fs';
+
+const database = require('../database')('io.orleans.wifi-setup.test');
+
+const should = chai.should();
+
+describe('Database', () => {
+    it('should init', (done) => {
+        database.init().then(() => {
+            done()
+            it('should start', (done) => {
+                database.init().start(() => {
+                    done()
+                    it('should store and retrieve wifi credentials', (done) => {
+                        let testSSID = 'testSSID';
+                        let testPassword = 'testPassword';
+                        database.setWifiCreds(testSSID, testPassword).then(() => {
+                            database.getWifiCreds().then((creds) => {
+                                creds.SSID.should.equal(testSSID);
+                                creds.password.should.equal(testPassword);
+                                done();
+                                it('should stop', (done) => {
+                                    database.stop().then(() => done());
+                                });
+                            });
+                        })
+                    });
+                });
+            });
+        });
+    });
+});
